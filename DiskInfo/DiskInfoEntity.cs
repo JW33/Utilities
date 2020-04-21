62b008377c514237950b2a5696899e8c535fc79a
Jeremy Wohltman <JW33@users.noreply.github.com>
2020-04-20 21:30:52 -0500

    Add files via upload

---
+++ DiskInfo/DiskInfoEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskInfo
+{
+    public class DiskInfoEntity
+    {
+        public string Description { get; set; }
+        public string Server { get; set; }
+
+        public string Name { get; set; }
+
+        public string DeviceId { get; set; }
+        public long FreeSpace { get; set; }
+
+        public long Size { get; set; }
+
+    }
+}
---
+++ DiskInfo/Email.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskInfo
+{
+    public enum EmailType
+    {
+        Info,
+        Alert
+    }
+
+    public class Email
+    {
+        static Logger logger = new Logger();
+
+        public void SendEmail(EmailType emailType, string message)
+        {
+            DateTime dt = DateTime.Now;
+            string emailAddresses = ConfigurationManager.AppSettings["emailAddresses"].ToString();
+            string fromAddress = ConfigurationManager.AppSettings["fromAddress"].ToString();
+
+            string infoSubject = ConfigurationManager.AppSettings["infoSubject"].ToString();
+            string infoBody = ConfigurationManager.AppSettings["infoBody"].ToString();
+
+            string alertSubject = ConfigurationManager.AppSettings["alertSubject"].ToString();
+            string alertBody = ConfigurationManager.AppSettings["alertBody"].ToString();
+
+            string smtpConfig = ConfigurationManager.AppSettings["smtpConfig"].ToString();
+
+            using (MailMessage mailMessage = new MailMessage(fromAddress, emailAddresses))
+            {
+                switch (emailType)
+                {
+                    case EmailType.Info:
+                        mailMessage.Subject = infoSubject;
+                        mailMessage.Body = infoBody;
+                        break;
+                    case EmailType.Alert:
+                        mailMessage.Subject = alertSubject;
+                        mailMessage.Body = alertBody;
+                        break;
+                    default:
+                        break;
+                }
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Host = smtpConfig;
+
+                    try
+                    {
+                        smtp.Send(mailMessage);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Log(LogType.Error, "Exception sending email", "SendMail()", ex.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
